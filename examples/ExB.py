4294da61efe69fe91aa5c1cea8ef8cc8dd0e94be
Cody Moynihan <cmoynih2@illinois.edu>
2020-10-02 13:29:33 -0500

    Some more changes

--- examples/ExB.py
+++ examples/ExB.py
@@ -13,7 +13,7 @@ solver = ParticleSolver(q=-qe, m=me)
 
 # Define electric field [V]
 def E(x,y,z):
-    Ex, Ey, Ez = 0., 0., 0.
+    Ex, Ey, Ez = 0., 300., 0.
     return Ex, Ey, Ez
 
 # Define magnetic field [T]
@@ -41,7 +41,7 @@ x,y,z = [r_L, 0., 0.]
 X0 = np.array([[x,y,z,vx,vy,vz]])
 
 # Times to solve for
-Nperiods = 1
+Nperiods = 5
 Nsteps_per_period = 50
 time = np.linspace(0., T_c*Nperiods, Nperiods*Nsteps_per_period)
 
@@ -55,16 +55,21 @@ z = solution[0,:,2]/r_L
 
 # Create figure for animation of motion
 fig,ax = plt.subplots()
-l, = ax.plot([],[], '-', color='g') # empty line that will be modified at each step (holds the previous points)
-s, = ax.plot([],[], marker='o', color='g') # empty line that will be modified at each step (holds current point)
-ax.set_xlim(1.25*x.min(), 1.25*x.max()) # set x limits
+N = 10
+lines = []
+for i in range(N):
+    lines.append(ax.plot([],[], marker='o')[0])
+
+l, = ax.plot([],[], '-') # empty line that will be modified at each step
+s, = ax.plot([],[], marker='o')
 ax.set_ylim(1.25*y.min(), 1.25*y.max()) # set y limits
+ax.set_xlim(1.25*x.min(), 1.25*x.max()) # set x limits
+ax.axvline(-1) # vertical line at -r_L
 ax.set_xlabel('$x/r_L$')
 ax.set_ylabel('$y/r_L$')
-ax.set_title('Cyclotron Motion of an Electron w/ $B = B_0 \hat{z}$')
+ax.set_title(r'$E \times B$ Motion w\ $E = E_0\hat{y}$, $B = B_0 \hat{z}$')
 
 # make aspect ratio 1. so circles appear correctly
-ax.set_box_aspect(1.)
 ax.set_aspect(1., adjustable='datalim')
 
 # function called to update plot at each time step
@@ -74,5 +79,5 @@ def update(i):
     s.set_data(x[i],y[i])
 
 # make the animation and display
-ani = FuncAnimation(fig, update, range(x.size), interval=50)
+ani = FuncAnimation(fig, update, range(x.size), interval = 50)
 plt.show()
